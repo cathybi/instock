6b1723680f851fe03443e9ee215147783b67e8f0
Tim Liu <timho1985@gmail.com>
2024-03-11 16:07:05 -0700

    J24BTW-4: create basic folder structure for server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const videos = require("./routes/videos");
+require("dotenv").config()
+const { PORT } = process.env;
+
+// Middleware
+app.use(express.json()); // sets the req.body
+app.use(express.static("public")); // make resources from the "public" folder available from the client
+app.use(cors()); // avoid CORS errors: allow clients from different domains to access server
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Instock server!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server starting on PORT:${PORT}`);
+});
