243659f1b73ba4ebc187cfd18fdb6e67194c001f
Tim Liu <timho1985@gmail.com>
2024-03-12 21:50:15 -0700

    Code review comments

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const cors = require("cors");
 const inventoriesRouter = require("./routes/inventoriesRouter");
 
 require("dotenv").config()
-const { PORT } = process.env;
+const { PORT } = process.env || 5050;
 
 // Middleware
 app.use(express.json()); // sets the req.body
 app.use(express.static("public")); // make resources from the "public" folder available from the client
 app.use(cors()); // avoid CORS errors: allow clients from different domains to access server
-
-app.get("/", (req, res) => {
-    res.send("Welcome to Instock server!");
-});
 
 app.use("/api/inventories", inventoriesRouter);
 
