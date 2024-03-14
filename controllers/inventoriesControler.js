1e0b3cdeb0d8087275ed2aea8aa9e14eeca55421
Trupti <patiltruptib@gmail.com>
2024-03-14 11:43:23 -0700

    solved Ticket #26 : Back-End: API to GET a Single Inventory Item

--- controllers/inventoriesControler.js
+++ controllers/inventoriesControler.js
@@ -5,7 +5,7 @@ const knex = require('knex')(require('../knexfile'));
  * @param {*} _req 
  * @param {*} res 
  */
-const getAll = async (_req, res) => {
+const index = async (_req, res) => {
   try {
     const data = await knex('inventories');
     res.status(200).json(data);
@@ -16,6 +16,20 @@ const getAll = async (_req, res) => {
   }
 }
 
+/**
+ * Returns single Inventory object for given id
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getSingleInventory = async(req, res) => {
+  try{
+    const inventory= await knex('inventories').where({id: req.params.id});
+    res.status(200).json(inventory[0]);
+  }catch(error) {
+    res.status(400).send(`Error in getting single Inventory: ${error}`)
+  }
+}
+
 module.exports = {
-  getAll
+  index
 }
