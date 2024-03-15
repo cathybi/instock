c87e148fde3d15a4dc657dedc676548e333d432a
Yingxin Bi <yingxinbi@Yingxins-MacBook-Pro.local>
2024-03-14 22:16:39 -0700

    create a new inventory

--- routes/inventoriesRouter.js
+++ routes/inventoriesRouter.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const inventoriesController = require('../controllers/inventoriesControler');
+const inventoriesController = require("../controllers/inventoriesControler");
 
 // Get the list of inventories
-router.route('/').get(inventoriesController.getInventoriesList);
+router
+  .route("/")
+  .get(inventoriesController.getInventoriesList)
+  .post(inventoriesController.addNewInventory); //create new inentory item
 
 //GET a Single Inventory Item
-router.route('/:id').get(inventoriesController.getSingleInventory);
+router.route("/:id").get(inventoriesController.getSingleInventory);
 
 //EDIT/PUT a Single Inventory Item
-router.route('/:id').put(inventoriesController.editSingleInventory);
+router.route("/:id").put(inventoriesController.editSingleInventory);
 
 module.exports = router;
